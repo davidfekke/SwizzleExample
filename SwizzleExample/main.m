fcdbadccb3c644d4bc7a95b52b71de0cb6390d16
David Fekke <david@fekke.com>
2021-09-29 09:21:31 -0400

    Initial Commit

---
+++ SwizzleExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwizzleExample
+//
+//  Created by David Fekke on 9/29/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
